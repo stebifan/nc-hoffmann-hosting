2b168cfc29539033a8fbdc462afdf6e56495199a
stebifan <stefan@tro-mail.de>
2018-06-03 18:22:19 +0200

    If Installed

--- config.sh
+++ config.sh
@@ -59,12 +59,12 @@ if version_greater "$image_version" "$installed_version"; then
     fi
 fi
 
-#if [[ $(find var/www/html/config/config.php -type f -size -100c 2>/dev/null) ]]; then
+if [ $(su - www-data -s /bin/sh -c "php /var/www/html/occ config:system:get installed" | grep -v true) ]; then
 	chown -cR www-data:root /var/www/html/themes
     su - www-data -s /bin/sh -c "php /var/www/html/occ maintenance:install -q -n --database-host "db" --database "pgsql" --database-name "nextcloud"  --database-user "nextcloud" --database-pass "$NC_POSTGRES_PASSWORD" --admin-user "$NEXTCLOUD_ADMIN_USER" --admin-pass "$NEXTCLOUD_ADMIN_PASSWORD" --data-dir "/var/www/html/data""
     su - www-data -s /bin/sh -c "php /var/www/html/occ config:system:set trusted_domains 2 --value="$DOMAIN""
     su - www-data -s /bin/sh -c "php /var/www/html/occ config:system:set theme --value="$THEME""
-#fi
+fi
 
 exec "$@"
 
